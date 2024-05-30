324ebc1a4f6f48621ef8312b20685f294c04ddef
PeriwinkleFTW <29137960+periwinkleFTW@users.noreply.github.com>
2024-05-30 06:43:48 -0600

    background and smoke effect

---
+++ components/HomeBackground.tsx
@@ -0,0 +1,47 @@
+import { ImageBackground, StyleSheet, Text, View, useWindowDimensions, Image, ScaledSize } from "react-native";
+import React from "react";
+import { Canvas, LinearGradient, Rect, vec } from "@shopify/react-native-skia";
+import useApplicationDimensions from "../hooks/useApplicationDimensions";
+
+const HomeBackground = () => {
+  const dimensions = useApplicationDimensions();
+  const { width, height } = dimensions;
+  const myStyles = styles(dimensions);
+  const smokeHeight = height * 0.6;
+  const smokeOffsetY = height * 0.4;
+
+  return (
+    <>
+      <Canvas style={{ flex: 1 }}>
+        <Rect x={0} y={0} width={width} height={height}>
+          <LinearGradient start={vec(0, 0)} end={vec(width, height)} colors={["#2E335A", "#1C1B33"]} />
+        </Rect>
+      </Canvas>
+      <ImageBackground source={require("../assets/home/Background.png")} resizeMode="cover" style={{ height: "100%" }}>
+        <Canvas style={{ height: smokeHeight, ...StyleSheet.absoluteFillObject, top: smokeOffsetY }}>
+          <Rect x={0} y={0} width={width} height={smokeHeight}>
+            <LinearGradient
+              start={vec(width / 2, 0)}
+              end={vec(width / 2, smokeHeight)}
+              colors={["rgba(58,63,84,0)", "rgba(58,63,84,1)"]}
+              positions={[-0.02, 0.54]}
+            />
+          </Rect>
+        </Canvas>
+        <Image source={require("../assets/home/House.png")} style={myStyles.houseImage} resizeMode="cover" />
+      </ImageBackground>
+    </>
+  );
+};
+
+export default HomeBackground;
+
+const styles = ({ width }: ScaledSize) =>
+  StyleSheet.create({
+    houseImage: {
+      width: width,
+      height: width,
+      ...StyleSheet.absoluteFillObject,
+      top: "36%",
+    },
+  });
